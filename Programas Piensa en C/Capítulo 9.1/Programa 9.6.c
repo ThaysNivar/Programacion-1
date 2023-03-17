ff82ef4e1490c2c9a7213fac399f2579626f61e3
ThaysNivar <123089059+ThaysNivar@users.noreply.github.com>
2023-03-17 05:52:16 -0400

    Add files via upload

---
+++ Programas Piensa en C/Capítulo 9.1/Programa 9.6.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+/* Archivos con variables enteras y reales.
+El programa lee datos de alumnos almacenados en un archivo y escribe la
+matricula y el promedio de cada alumno.
---
+++ Programas Piensa en C/Capítulo 9.2/Programa 9.15.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/*Letras minusculas y mayusculas.
+El programa, al recibir como dato un archivo formado por cadenas de
+caracteres, determina el número de letras minusculas y mayusculas que hay
+en el archivo. */
+void minymay(FILE *);      /*Prototipo de función. */
+
+void main(void)
+{
+FILE *ap;
+if ((ap = fopen("arc5.txt", "r")) != NULL)
+{
+    minymay(ap);
+    fclose(ap);
+}
+else
+    printf("No se puedo abrir el archivo");
+}
+
+void minymay(FILE *ap1)
+/*Esta funcion se utiliza para leer cadenas de caracteres de un archivo
+y contar el numero de letras minúsculas y mayúsculas que existen en el
+archivo. */
+{
+char cad[30];
+int i, mi =0, ma = 0;
+while (!feof(ap1))
+{
+    fgets(cad, 30, ap1);
+    /*Se utiliza la funcion fgets() para leer cadenas de caracteres del
+    archivo. */
+    i=0;
+    while (cad[i] != '\0')
+    {
+        if (islower(cad[i]))
+            mi++;
+        else
+            if (isupper(cad[i]))
+                ma++;
+        i++;
+    }
+}
+printf("\n\nNumero de letras minusculas: %d", mi);
+printf("\nNumero de letras mayusculas: %d", ma);
+}
